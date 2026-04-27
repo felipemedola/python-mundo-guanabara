7804ac418f2b830a3749271db3e7c7ca81abe614
felipemedola <felipe.medola@hotmail.com>
2026-04-27 05:57:49 -0300

    update: melhorias no código do exercício 50

--- mundo-02/ex050.py
+++ mundo-02/ex050.py
@@ -7,4 +7,4 @@ for c in range(0, 6):
         total += num
     cont += 1
 
-print(f'Você informou {cont} números e a soma foi {total}.')
+print(f'Você informou {cont} números PARES e a soma foi {total}.')
